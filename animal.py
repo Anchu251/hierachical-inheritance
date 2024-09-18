61cade02116ec09ee23789666d9df9bdb3ed53b9
Anchu251 <179801580+Anchu251@users.noreply.github.com>
2024-09-18 09:14:29 +0700

    Add files via upload

--- animal.py
+++ animal.py
@@ -1,4 +1,3 @@
 class Animal:
     def eat(self):
-        return 'eating'
-    
+        return 'eating...'
--- dog.py
+++ dog.py
@@ -1,4 +1,5 @@
 from animal import Animal
 class Dog(Animal):
     def bark(self):
-        return 'barking'
+        return 'barking...'
+    
